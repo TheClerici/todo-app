634ffbbbb6804ea8b9347ec223d8abbc855c6ed2
TheClerici <enrique_cle97@hotmail.com>
2022-11-22 01:08:19 -0700

    stats

--- src/components/stats/Stats.js
+++ src/components/stats/Stats.js
@@ -8,28 +8,34 @@ const Stats = (props) => {
       <div className="stats__avg">
         <label>Average time to finish All tasks:</label>
       </div>
-      <div>
+      <div className="stats__stats">
         <label>
           {props.items[0]} Minutes and {props.items[1]} Seconds.
         </label>
       </div>
-      <label className="stats-box-1">Avg Low priority:</label>
-      <div>
-        <label className="stats-box-1">
-          {props.items[2]} Min and {props.items[3]} Sec.
-        </label>
+      <div className="stats-box-1">
+        <label>Avg Low priority:</label>
+        <div>
+          <label>
+            {props.items[2]} Min and {props.items[3]} Sec.
+          </label>
+        </div>
       </div>
-      <label className="stats-box-2">Avg Medium priority:</label>
-      <div>
-        <label className="stats-box-2">
-          {props.items[4]} Min and {props.items[5]} Sec.
-        </label>
+      <div className="stats-box-2">
+        <label>Avg Medium priority:</label>
+        <div>
+          <label>
+            {props.items[4]} Min and {props.items[5]} Sec.
+          </label>
+        </div>
       </div>
-      <label className="stats-box-3">Avg High priority:</label>
-      <div>
-        <label className="stats-box-3">
-          {props.items[6]} Min and {props.items[7]} Sec.
-        </label>
+      <div className="stats-box-3">
+        <label>Avg High priority:</label>
+        <div>
+          <label>
+            {props.items[6]} Min and {props.items[7]} Sec.
+          </label>
+        </div>
       </div>
     </div>
   );
